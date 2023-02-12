f19e5bfc53f8b58e217ddad4f1de99b2f8b75d60
rabo8497 <jeon9799@gmail.com>
2023-02-12 18:22:37 +0900

    set

--- video_sync/server.js
+++ video_sync/server.js
@@ -7,9 +7,7 @@ const fs = require('fs');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-let current_time = 0; 
-let is_play = false;
-let vid_link;
+
 app.use(express.static(__dirname + '/video'));
 
 app.get('/', (req, res) => {
@@ -19,33 +17,20 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('a user connected');
 
-    socket.on('video_change', (link) => {
-        vid_link = link
-        socket.broadcast.emit('video_change_from_server', link);
-    });
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 
     socket.on('seek', (time) => {
-        current_time = time
         socket.broadcast.emit('seek', time);
     });
 
-    socket.on('play', (time) => {
-        is_play = true
-        current_time = time
+    socket.on('play', () => {
         socket.broadcast.emit('play');
     });
 
-    socket.on('pause', (time) => {
-        is_play = false
-        current_time = time
+    socket.on('pause', () => {
         socket.broadcast.emit('pause');
-    });
-
-    socket.on('sync_others', () => {
-        socket.emit('sync_to_me_other', vid_link, is_play, current_time);
     });
 });
 
